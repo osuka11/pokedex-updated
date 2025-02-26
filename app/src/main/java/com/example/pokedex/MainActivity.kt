6b6650925e704c1ee70e1827ef291ab55c267559
Oscar Rincon <orincon0@ucol.mx>
2025-02-25 21:59:28 -0700

    Project Created

---
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.pokedex
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.pokedex.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+}
